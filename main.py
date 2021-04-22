8306955477e5f08b6d8e57573f37bbfb1849532b
ItsYaBoiSamy <belangersa@educ.college-montreal.qc.ca>
2021-04-22 14:53:58 -0400

    Tic Tac Toe

--- main.py
+++ main.py
@@ -1,145 +1,190 @@
-"""
-Modèle de départ pour la programmation Arcade.
-Il suffit de modifier les méthodes nécessaires à votre jeu.
-"""
 import arcade
-import random
+import math
+
+ScreenWidth = 1680
+ScreenHeight = 850
+X_image = arcade.load_texture("Sprites/X.png")
+O_image = arcade.load_texture("Sprites/O.png")
 
 
-SCREEN_WIDTH = 1680
-SCREEN_HEIGHT = 850
-SCREEN_TITLE = "Projet TP1"
-COLORS = [arcade.color.BLUE, arcade.color.PURPLE,
-          arcade.color.GREEN, arcade.color.RED,
-          arcade.color.YELLOW, arcade.color.ORANGE,
-          arcade.color.WHITE, arcade.color.DIM_GRAY,
-          arcade.color.PALE_BLUE, arcade.color.PINK,
-          arcade.color.BROWN]
+class TicTacToe_game:
+    def __init__(self, grid_x, grid_y, square_size, grid_color):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.square_size = square_size
+        self.bar_width = self.square_size / 10
+        self.grid_color = grid_color
+        self.bar_length = self.square_size * 3 + self.bar_width * 2
+        # Predefined relative coordinates for all of the tic tac toe squares
+        self.winner = "no one"
+        self.assigned_square_pos = [
+            (self.grid_x - self.square_size - self.bar_width, self.grid_y - self.square_size - self.bar_width),
+            (self.grid_x, self.grid_y - self.square_size - self.bar_width),
+            (self.grid_x + self.square_size + self.bar_width, self.grid_y - self.square_size - self.bar_width),
+            (self.grid_x - self.square_size - self.bar_width, self.grid_y),
+            (self.grid_x, self.grid_y),
+            (self.grid_x + self.square_size + self.bar_width, self.grid_y),
+            (self.grid_x - self.square_size - self.bar_width, self.grid_y + self.square_size + self.bar_width),
+            (self.grid_x, self.grid_y + self.square_size + self.bar_width),
+            (self.grid_x + self.square_size + self.bar_width, self.grid_y + self.square_size + self.bar_width)]
+        self.square_list = []
+        for coordinate in self.assigned_square_pos:
+            self.square_list.append(TicTacToe_square(coordinate[0], coordinate[1], self.square_size))
+        self.turn = 'X'
 
-#Cette fonction génère un nombre aléatoire entre les bornes désignées, le nombre 0 est exclu des possibilitées aléatoires
-def random_without_zero(num1, num2):
-    random_result = 0
-    while random_result == 0:
-        random_result = random.randint(num1, num2)
-    return random_result
+    def draw_grid(self):
+        arcade.draw_rectangle_filled(self.grid_x, self.grid_y + (self.bar_width + self.square_size) / 2,
+                                     self.bar_length, self.bar_width, self.grid_color)
+        arcade.draw_rectangle_filled(self.grid_x, self.grid_y - (self.bar_width + self.square_size) / 2,
+                                     self.bar_length, self.bar_width, self.grid_color)
+        arcade.draw_rectangle_filled(self.grid_x + (self.bar_width + self.square_size) / 2, self.grid_y, self.bar_width,
+                                     self.bar_length, self.grid_color)
+        arcade.draw_rectangle_filled(self.grid_x - (self.bar_width + self.square_size) / 2, self.grid_y, self.bar_width,
+                                     self.bar_length, self.grid_color)
+        for square in self.square_list:
+            square.draw_square()
+        if self.winner == "X":
+            arcade.draw_text("X WINS", self.grid_x, self.grid_y + self.bar_length / 2 + 20, arcade.color.RED, 40,
+                             width=200, align="center", anchor_x="center", anchor_y="center")
+        elif self.winner == "O":
+            arcade.draw_text("O WINS", self.grid_x, self.grid_y + self.bar_length / 2 + 20, arcade.color.BLUE, 40,
+                             width=200, align="center", anchor_x="center", anchor_y="center")
+        elif self.winner == "stalemate":
+            arcade.draw_text("STALEMATE", self.grid_x, self.grid_y + self.bar_length / 2 + 20, arcade.color.DIM_GRAY, 40,
+                             width=300, align="center", anchor_x="center", anchor_y="center")
 
-#Cette classe permet la création de cercles, elle est appelée chaque fois que le joueur pèse sur le bouton gauche de la souris
-class Cercle ():
-    def __init__(self, pos_x, pos_y):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.rayon = random.randint(10, 30)
-        self.couleur = random.choice(COLORS)
-        self.speed_x = random.randint(-3, 3)
-        self.speed_y = random.randint(-3, 3)
+    def mouse_position_verification_grid(self, mouse_x, mouse_y):
+        if self.winner == "no one":
+            for square in self.square_list:
+                square.mouse_position_verification_square(mouse_x, mouse_y)
 
-    #Cette méthode gère le rendering du cercle, elle est appelée à chaque frame
-    def draw_cercle (self):
-        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rayon, self.couleur)
+    def mouse_click_verification_grid(self, mouseButton):
+        if self.winner == "no one":
+            for square in self.square_list:
+                self.turn = square.mouse_click_verification_square(mouseButton, self.turn)
+            if (self.square_list[0].square_state == self.square_list[1].square_state and self.square_list[
+                1].square_state == self.square_list[2].square_state and self.square_list[0].square_state == 2) or (
+                    self.square_list[3].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[5].square_state and self.square_list[3].square_state == 2) or (
+                    self.square_list[6].square_state == self.square_list[7].square_state and self.square_list[
+                7].square_state == self.square_list[8].square_state and self.square_list[6].square_state == 2) or (
+                    self.square_list[0].square_state == self.square_list[3].square_state and self.square_list[
+                3].square_state == self.square_list[6].square_state and self.square_list[0].square_state == 2) or (
+                    self.square_list[1].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[7].square_state and self.square_list[1].square_state == 2) or (
+                    self.square_list[2].square_state == self.square_list[5].square_state and self.square_list[
+                5].square_state == self.square_list[8].square_state and self.square_list[2].square_state == 2) or (
+                    self.square_list[0].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[8].square_state and self.square_list[0].square_state == 2) or (
+                    self.square_list[2].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[6].square_state and self.square_list[2].square_state == 2):
+                self.winner = "X"
 
-    #Cette méthode gère les ajustements dans la position du cercle, elle est appelée à chaque frame
-    def update_cercle (self):
-        if self.pos_x + self.rayon >= SCREEN_WIDTH or self.pos_x - self.rayon <= 0:
-            self.speed_x *= -1
-        if self.pos_y + self.rayon >= SCREEN_WIDTH or self.pos_y - self.rayon <= 0:
-            self.speed_y *= -1
-        self.pos_x += self.speed_x
-        self.pos_y += self.speed_y
+            elif (self.square_list[0].square_state == self.square_list[1].square_state and self.square_list[
+                1].square_state == self.square_list[2].square_state and self.square_list[0].square_state == 3) or (
+                    self.square_list[3].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[5].square_state and self.square_list[3].square_state == 3) or (
+                    self.square_list[6].square_state == self.square_list[7].square_state and self.square_list[
+                7].square_state == self.square_list[8].square_state and self.square_list[6].square_state == 3) or (
+                    self.square_list[0].square_state == self.square_list[3].square_state and self.square_list[
+                3].square_state == self.square_list[6].square_state and self.square_list[0].square_state == 3) or (
+                    self.square_list[1].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[7].square_state and self.square_list[1].square_state == 3) or (
+                    self.square_list[2].square_state == self.square_list[5].square_state and self.square_list[
+                5].square_state == self.square_list[8].square_state and self.square_list[2].square_state == 3) or (
+                    self.square_list[0].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[8].square_state and self.square_list[0].square_state == 3) or (
+                    self.square_list[2].square_state == self.square_list[4].square_state and self.square_list[
+                4].square_state == self.square_list[6].square_state and self.square_list[2].square_state == 3):
+                self.winner = "O"
 
-#Cette classe permet la création de rectangles, elle est appelée chaque fois que le joueur pèse sur le bouton droit de la souris
-class Rect ():
-    def __init__(self, pos_x, pos_y):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.largeur = random.randint(20, 60)
-        self.hauteur = random.randint(20, 60)
-        self.couleur = random.choice(COLORS)
-        self.speed_x = random_without_zero(-3, 3)
-        self.speed_y = random_without_zero(-3, 3)
+            elif (self.square_list[0].square_state == 2 or self.square_list[0].square_state == 3) and (
+                    self.square_list[1].square_state == 2 or self.square_list[1].square_state == 3) and (
+                    self.square_list[2].square_state == 2 or self.square_list[2].square_state == 3) and (
+                    self.square_list[3].square_state == 2 or self.square_list[3].square_state == 3) and (
+                    self.square_list[4].square_state == 2 or self.square_list[4].square_state == 3) and (
+                    self.square_list[5].square_state == 2 or self.square_list[5].square_state == 3) and (
+                    self.square_list[6].square_state == 2 or self.square_list[6].square_state == 3) and (
+                    self.square_list[7].square_state == 2 or self.square_list[7].square_state == 3) and (
+                    self.square_list[8].square_state == 2 or self.square_list[8].square_state == 3):
+                self.winner = "stalemate"
 
-    # Cette méthode gère le rendering du rectangle, elle est appelée à chaque frame
-    def draw_rect (self):
-        arcade.draw_rectangle_filled(self.pos_x, self.pos_y, self.largeur, self.hauteur, self.couleur)
 
-    # Cette méthode gère les ajustements dans la position du rectangle, elle est appelée à chaque frame
-    def update_rect (self):
-        if self.pos_x + self.largeur >= SCREEN_WIDTH or self.pos_x <= 0:
-            self.speed_x *= -1
-        if self.pos_y + self.hauteur >= SCREEN_HEIGHT or self.pos_y <= 0:
-            self.speed_y *= -1
-        self.pos_x += self.speed_x
-        self.pos_y += self.speed_y
+class TicTacToe_square:
+    def __init__(self, square_x, square_y, square_size):
+        # State 0 is neutral, nothing is happening in that situation
+        # State 1 is the state where the mouse is hovering over the square, the square is a light shade of gray
+        # State 2 is X
+        # State 3 is O
+        self.square_size = square_size
+        self.square_state = 0
+        self.square_x = square_x
+        self.square_y = square_y
 
-class MyGame(arcade.Window):
-    """
-    La classe principale de l'application
+    def mouse_position_verification_square(self, mouse_x, mouse_y):
+        if -1 * self.square_size / 2 < mouse_x - self.square_x < self.square_size / 2 \
+                and -1 * self.square_size / 2 < mouse_y - self.square_y < self.square_size / 2 \
+                and (self.square_state == 0 or self.square_state == 1):
+            self.square_state = 1
+        elif self.square_state == 1:
+            self.square_state = 0
 
-    NOTE: Vous pouvez effacer les méthodes que vous n'avez pas besoin.
-    Si vous en avez besoin, remplacer le mot clé "pass" par votre propre code.
-    """
+    def mouse_click_verification_square(self, mouseButton, turn):
+        if self.square_state == 1 and mouseButton == arcade.MOUSE_BUTTON_LEFT:
+            if turn == 'X':
+                self.square_state = 2
+                return 'O'
+            else:
+                self.square_state = 3
+                return 'X'
+        else:
+            return turn
+
+    def draw_square(self):
+        if self.square_state == 1:
+            arcade.draw_rectangle_filled(self.square_x, self.square_y, self.square_size, self.square_size,
+                                         arcade.color.GAINSBORO)
+        elif self.square_state == 2:
+            arcade.draw_texture_rectangle(self.square_x, self.square_y, self.square_size - 30, self.square_size - 30,
+                                          X_image)
+        elif self.square_state == 3:
+            arcade.draw_texture_rectangle(self.square_x, self.square_y, self.square_size - 30, self.square_size - 30,
+                                          O_image)
+
+
+class MakeGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.BLACK)
+        self.grid_list = []
 
-    #Cette méthode initialise les listes de cercles et de rectangles, elle est appelée une fois quand le jeu est débuté
     def setup(self):
-        """
-        Configurer les variables de votre jeu ici. Il faut appeler la méthode une nouvelle
-        fois si vous recommencer une nouvelle partie.
-        """
-        self.liste_cercles = []
-        self.liste_rectangles = []
-        self.rapid_fire = False
+        self.grid_list.append(TicTacToe_game(ScreenWidth / 4, ScreenHeight / 4, 100, arcade.color.BLACK))
+        self.grid_list.append(TicTacToe_game(ScreenWidth / 2, ScreenHeight / 4, 100, arcade.color.BLACK))
+        self.grid_list.append(TicTacToe_game(ScreenWidth * 3 / 4, ScreenHeight / 4, 100, arcade.color.BLACK))
+        self.grid_list.append(TicTacToe_game(ScreenWidth / 4, ScreenHeight * 3 / 4, 100, arcade.color.BLACK))
+        self.grid_list.append(TicTacToe_game(ScreenWidth / 2, ScreenHeight * 3 / 4, 100, arcade.color.BLACK))
+        self.grid_list.append(TicTacToe_game(ScreenWidth * 3 / 4, ScreenHeight * 3 / 4, 100, arcade.color.BLACK))
 
-    #Cette méthode s'occupe du rendering
     def on_draw(self):
-        """
-        C'est la méthode que Arcade invoque à chaque "frame" pour afficher les éléments
-        de votre jeu à l'écran.
-        """
         arcade.start_render()
-        for cercle in self.liste_cercles:
-            cercle.draw_cercle()
+        arcade.set_background_color(arcade.color.WHITE)
+        for grid in self.grid_list:
+            grid.draw_grid()
+        arcade.finish_render()
 
-        for rect in self.liste_rectangles:
-            rect.draw_rect()
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        for grid in self.grid_list:
+            grid.mouse_position_verification_grid(x, y)
 
-    #Cette méthode s'occupe des ajustements de position des formes, elle est appelée à chaque frame
-    def on_update(self, delta_time):
-        """
-        Toute la logique pour déplacer les objets de votre jeu et de
-        simuler sa logique vont ici. Normalement, c'est ici que
-        vous allez invoquer la méthode "update()" sur vos listes de sprites.
-        Paramètre:
-            - delta_time : le nombre de milliseconde depuis le dernier update.
-        """
-        for cercle in self.liste_cercles:
-            cercle.update_cercle()
-
-        for rect in self.liste_rectangles:
-            rect.update_rect()
-
-    #Cette méthode crée une forme dépendemment du bonton de la souris pesé, le bouton droit crée un cercle tandis que le bouton gauche crée un rectangle
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Méthode invoquée lorsque l'usager clique un bouton de la souris.
-        Paramètres:
-            - x, y: coordonnées où le bouton a été cliqué
-            - button: le bouton de la souris appuyé
-            - key_modifiers: est-ce que l'usager appuie sur "shift" ou "ctrl" ?
-        """
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.liste_cercles.append(Cercle(x, y))
-        if button == arcade.MOUSE_BUTTON_RIGHT:
-            self.liste_rectangles.append(Rect(x, y))
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        for grid in self.grid_list:
+            grid.mouse_click_verification_grid(button)
 
 
 def main():
-    """ Main method """
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    TicTacToe = MakeGame(ScreenWidth, ScreenHeight, "Tic Tac Toe")
+    TicTacToe.setup()
     arcade.run()
 
 
-if __name__ == "__main__":
-    main()
+main()
